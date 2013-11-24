586bdfb078dc6c59b7c206b1591e76adb205aca0
Cory Gabrielsen <corygabrielsen@gmail.com>
2013-11-24 01:43:03 -0800

    Reorganized SimpleTodoLogger

--- SimpleTodo/src/main/java/com/example/thetodoapp/util/SimpleTodoLogger.java
+++ SimpleTodo/src/main/java/com/example/thetodoapp/util/SimpleTodoLogger.java
@@ -9,18 +9,22 @@ import android.util.Log;
 import com.example.thetodoapp.SimpleTodoApp;
 
 /**
- * A thin wrapper around {@Link Log} to always log to {@link com.example.thetodoapp.SimpleTodoApp#TAG}
+ * A thin wrapper around {@Link Log} to always log to {@link SimpleTodoApp#TAG}
  */
 public class SimpleTodoLogger {
 
-    private static final String TAG = SimpleTodoApp.TAG;
+    /*
+     * --------------------
+     * Debug
+     * --------------------
+     */
 
     /**
      * Sends a {@link Log#DEBUG} message.
      * @param msg The message you would like logged
      */
     public static int d(final String msg) {
-        return Log.d(TAG, msg);
+        return Log.d(SimpleTodoApp.TAG, msg);
     }
 
     /**
@@ -29,15 +33,21 @@ public class SimpleTodoLogger {
      * @param tr An exception to log
      */
     public static int d(final String msg, final Throwable tr) {
-        return Log.d(TAG, msg, tr);
+        return Log.d(SimpleTodoApp.TAG, msg, tr);
     }
+
+    /*
+     * --------------------
+     * Error
+     * --------------------
+     */
 
     /**
      * Sends a {@link Log#ERROR} message.
      * @param msg The message you would like logged
      */
     public static int e(final String msg) {
-        return Log.e(TAG, msg);
+        return Log.e(SimpleTodoApp.TAG, msg);
     }
 
     /**
@@ -46,23 +56,21 @@ public class SimpleTodoLogger {
      * @param tr An exception to log
      */
     public static int e(final String msg, final Throwable tr) {
-        return Log.e(TAG, msg, tr);
+        return Log.e(SimpleTodoApp.TAG, msg, tr);
     }
 
-    /**
-     * Handy function to get a loggable stack trace from a {@link Throwable}.
-     * @param tr An exception to log
+    /*
+     * --------------------
+     * Info
+     * --------------------
      */
-    public static String getStackTraceString(final Throwable tr) {
-        return Log.getStackTraceString(tr);
-    }
 
     /**
      * Sends a {@link Log#INFO} message.
      * @param msg The message you would like logged
      */
     public static int i(final String msg) {
-        return Log.i(TAG, msg);
+        return Log.i(SimpleTodoApp.TAG, msg);
     }
 
     /**
@@ -71,43 +79,21 @@ public class SimpleTodoLogger {
      * @param tr An exception to log
      */
     public static int i(final String msg, Throwable tr) {
-        return Log.i(TAG, msg);
+        return Log.i(SimpleTodoApp.TAG, msg);
     }
 
-    /**
-     * Checks to see whether or not a log for the specified tag is loggable at the specified level.
-     * The default level of any tag is set to {@link Log#INFO}. This means that any level above and
-     * including {@link Log#INFO} will be logged. Before you make any calls to a logging method you
-     * should check to see if your tag should be logged. You can change the default level by setting
-     * a system property: 'setprop log.tag.<YOUR_LOG_TAG> <LEVEL>' Where level is either
-     * {@link Log#VERBOSE}, {@link Log#DEBUG}, {@link Log#INFO}, {@link Log#WARN},
-     * {@link Log#ERROR}, {@link Log#ASSERT}, or SUPPRESS. SUPPRESS will turn off all logging for
-     * your tag. You can also create a local.prop file that with the following in it:
-     * 'log.tag.<YOUR_LOG_TAG>=<LEVEL>' and place that in /data/local.prop.
-     * @param level The level to check
-     * @return Whether or not that this is allowed to be logged.
-     * @throws java.lang.IllegalArgumentException is thrown if the {@link #TAG}.length() > 23
+    /*
+     * --------------------
+     * Verbose
+     * --------------------
      */
-    public static boolean isLoggable(final int level) {
-        return Log.isLoggable(TAG, level);
-    }
-
-    /**
-     * Low-level logging call.
-     * @param priority The priority/type of this log message
-     * @param msg The message you would like logged
-     * @return The number of bytes written
-     */
-    public static int println(final int priority, final String msg) {
-        return Log.println(priority, TAG, msg);
-    }
 
     /**
      * Sends a {@link Log#VERBOSE} message.
      * @param msg The message you would like logged
      */
     public static int v(final String msg) {
-        return Log.v(TAG, msg);
+        return Log.v(SimpleTodoApp.TAG, msg);
     }
 
     /**
@@ -116,12 +102,18 @@ public class SimpleTodoLogger {
      * @param tr An exception to log
      */
     public static int v(final String msg, final Throwable tr) {
-        return Log.v(TAG, msg, tr);
+        return Log.v(SimpleTodoApp.TAG, msg, tr);
     }
+
+    /*
+     * --------------------
+     * Warn
+     * --------------------
+     */
 
     // Log API has no comment
     public static int w(final Throwable tr) {
-        return Log.w(TAG, tr);
+        return Log.w(SimpleTodoApp.TAG, tr);
     }
 
 
@@ -131,7 +123,7 @@ public class SimpleTodoLogger {
      * @param tr An exception to log
      */
     public static int w(final String msg, final Throwable tr) {
-        return Log.w(TAG, msg, tr);
+        return Log.w(SimpleTodoApp.TAG, msg, tr);
     }
 
     /**
@@ -139,8 +131,14 @@ public class SimpleTodoLogger {
      * @param msg The message you would like logged
      */
     public static int w(final String msg) {
-        return Log.w(TAG, msg);
+        return Log.w(SimpleTodoApp.TAG, msg);
     }
+
+    /*
+     * --------------------
+     * Wtf
+     * --------------------
+     */
 
     /**
      * What a Terrible Failure: Report an exception that should never happen.
@@ -148,7 +146,7 @@ public class SimpleTodoLogger {
      * @param tr An exception to log
      */
     public static int wtf(final Throwable tr) {
-        return Log.wtf(TAG, tr);
+        return Log.wtf(SimpleTodoApp.TAG, tr);
     }
 
     /**
@@ -159,7 +157,7 @@ public class SimpleTodoLogger {
      * @param msg The message you would like logged
      */
     public static int wtf(final String msg) {
-        return Log.d(TAG, msg);
+        return Log.d(SimpleTodoApp.TAG, msg);
     }
 
     /**
@@ -169,6 +167,48 @@ public class SimpleTodoLogger {
      * @param tr An exception to log. May be null.
      */
     public static int wtf(final String msg, final Throwable tr) {
-        return Log.wtf(TAG, msg, tr);
+        return Log.wtf(SimpleTodoApp.TAG, msg, tr);
+    }
+
+    /*
+     * --------------------
+     * Other android.util.Log methods
+     * --------------------
+     */
+    /**
+     * Handy function to get a loggable stack trace from a {@link Throwable}.
+     * @param tr An exception to log
+     */
+    public static String getStackTraceString(final Throwable tr) {
+        return Log.getStackTraceString(tr);
+    }
+
+
+    /**
+     * Checks to see whether or not a log for the specified tag is loggable at the specified level.
+     * The default level of any tag is set to {@link Log#INFO}. This means that any level above and
+     * including {@link Log#INFO} will be logged. Before you make any calls to a logging method you
+     * should check to see if your tag should be logged. You can change the default level by setting
+     * a system property: 'setprop log.tag.<YOUR_LOG_SimpleTodoApp.TAG> <LEVEL>' Where level is either
+     * {@link Log#VERBOSE}, {@link Log#DEBUG}, {@link Log#INFO}, {@link Log#WARN},
+     * {@link Log#ERROR}, {@link Log#ASSERT}, or SUPPRESS. SUPPRESS will turn off all logging for
+     * your tag. You can also create a local.prop file that with the following in it:
+     * 'log.tag.<YOUR_LOG_SimpleTodoApp.TAG>=<LEVEL>' and place that in /data/local.prop.
+     * @param level The level to check
+     * @return Whether or not that this is allowed to be logged.
+     * @throws java.lang.IllegalArgumentException is thrown if the {@link SimpleTodoApp#TAG}.length() > 23
+     */
+    public static boolean isLoggable(final int level) {
+        return Log.isLoggable(SimpleTodoApp.TAG, level);
+    }
+
+    /**
+     * Low-level logging call.
+     * @param priority The priority/type of this log message
+     * @param msg The message you would like logged
+     * @return The number of bytes written
+     */
+    public static int println(final int priority, final String msg) {
+        return Log.println(priority, SimpleTodoApp.TAG, msg);
     }
 }
--- SimpleTodo/src/main/java/com/example/thetodoapp/util/Utils.java
+++ SimpleTodo/src/main/java/com/example/thetodoapp/util/Utils.java
@@ -11,6 +11,10 @@ import android.view.inputmethod.InputMethodManager;
  */
 public class Utils {
 
+    /**
+     * Closes the keyboard
+     * @param c
+     */
     public static void closeKeyboard(final Context c) {
         ((InputMethodManager)
                 c.getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(0, 0);
